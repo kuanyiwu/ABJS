68487d2b2e2d58e85f681129040cfa0791121139
Kuanyi Wu <kuanyiwu@usc.edu>
2018-05-25 11:38:45 -0400

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -165,7 +165,7 @@ def job_family_roles_intent(intent, session):
         q = intent['slots']['family']['value']
         
         #synonyms
-        if intent['slots']['family']['resolutions']['resolutionsPerAuthority'][0]['values'][0]['value']['name'] != None:
+        if 'values' in intent['slots']['family']['resolutionsPerAuthority'][0]:
             q = intent['slots']['family']['resolutions']['resolutionsPerAuthority'][0]['values'][0]['value']['name']
             
         session_attributes = create_intent_attributes(q)
@@ -198,7 +198,7 @@ def job_family_roles_intent(intent, session):
         q = intent['slots']['role']['value']
         
         #synonyms
-        if intent['slots']['role']['resolutions']['resolutionsPerAuthority'][0]['values'][0]['value']['name'] != None:
+        if 'resolutions' in intent['slots']['role']:
             q = intent['slots']['role']['resolutions']['resolutionsPerAuthority'][0]['values'][0]['value']['name']
             
         session_attributes = create_intent_attributes(q)
@@ -247,6 +247,28 @@ def job_family_roles_intent(intent, session):
             speech_output = "A project manager manages, plans, organizes and implements one or more i.t. Projects using standard methodology. "
         elif q == "project management office":
             speech_output = "A p.m.o provides support for financials tracking and change request management while also supporting stakeholder and business client management."
+        elif q == "test manager":
+            speech_output = " A test manager plan, coordinate and control the entire test process within one or more projects. They also are responsible for managing, implementing and monitoring all test efforts while also ensuring all activities met with guidelines and are compliant with rules and regulations."
+        elif q == "test engineer":
+            speech_output = "A test engineer execute testing inline with the test strategy and approach and are there to support the test manager to ensure all requested technical facilitation are setup as defined in the test plan."
+        elif q == "quality manager":
+            speech_output = "A quality manager proposes and define quality objectives to meet customer needs and required product quality as well as contribute to quality management and quality improvement initiatives and ensure all management is compliant with c.s. Standards and policies. "
+        elif q == "account manager":
+            speech_output = "The account manager are in charge with owning the account portfolio and ensuring an appropriate balance between clients and the i.t. Supplier concerns, explain service, cost metrics and related trends to clients."
+        elif q == "delivery manager":
+            speech_output = "A delivery manager manage the delivery of i.t. Infrastructure or application development solutions, ensure a clear understanding of client objectives as well as plan and coordinate and ensure timely and on-budget delivery."
+        elif q == "service manager":
+            speech_output = "The service manager manages an individual or group of i.t. Services and the achievement of respective s.l.a.s. They work and partner with all internal and external service providers to ensure that the i.t. Services are continuously delivered and accurately reported."
+        elif q == "product manager":
+            speech_output = "A product manager provide the market mind-set required for the outcome based definition of services, build a detailed understanding of customer and user requirements, drive continuous improvement for products and balance change and innovation."
+        elif q == "infrastructure architect":
+            speech_output = "The infrastructure architect define the strategy and architecture for a technology domains, define the long term strategy and target architecture and drive innovation by exploring and testing new technologies that drive value."
+        elif q == "service desk":
+            speech_output = "The service desk act as a single point of contact, log all incoming calls to the service desk, contribute to the knowledge base and input to major incident process."
+        elif q == "desktop support":
+            speech_output = "A desktop support communicate request and incident status to relevant stakeholders, monitors progress of ticket in accordance with defined service level agreements and more."
+        elif q == "application support":
+            speech_output = "The application support analyze incident and problem trends and propose proactive measures to improve stability and improve efficiency, while also managing vendor relationships and act as single point of contact for all escalated operational service delivery issues."
         else:
             speech_output = "I'm not sure what you're asking"
         reprompt_text = "You can ask me what are the job families at cs"
@@ -325,6 +347,122 @@ def track_job_families_intent(intent, session):
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
+#JobCert Intent
+def job_cert_intent(intent, session):
+
+    card_title = 'cert'
+    session_attributes = {}
+    should_end_session = False
+
+    if 'family' in intent['slots']:
+        q = intent['slots']['family']['value']
+        
+        #synonyms
+        if intent['slots']['family']['resolutions']['resolutionsPerAuthority'][0]['values'][0]['value']['name'] != None:
+            q = intent['slots']['family']['resolutions']['resolutionsPerAuthority'][0]['values'][0]['value']['name']
+            
+        session_attributes = create_intent_attributes(q)
+        
+        if q == "application architecture and development":
+            speech_output = "Some certificates that an a.d. Can receive is the b.c.s.f.l.s.d or the o.r.a.j.s.e.7. Or the i.e.e.e.c.s.d.p.."
+        elif q == "business analysis and engineering":
+            speech_output = "Some certificates that an b.e. Can receive are the c.b.a.p.. The C.b.i.p.. The b.c.s.f."
+        elif q == "business management":
+            speech_output = "The certificates that a b.m. can receive are the c.p.a. Or the a.c.c.a.. As well as the e.b.."
+        elif q == "project management":
+            speech_output = "The certificates that a p.m.. can gain range from the c.a..p.m to the p.r.i.2.p.."
+        elif q == "quality management and testing":
+            speech_output = "The certificates that a q.m. can receive are the s.q.e., the s.a.q.e.o.q. And also the i.s.t.q.b.e.x.p.."
+        else:
+            speech_output = "Try asking me about certificates for a.d., b.e., b.m., q.m, or p.m."
+        reprompt_text = "You can ask me what are the job families at cs"
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+def jf_extra_help(intent, session):
+	card_title = 'Resources'
+	session_attributes = {}
+	should_end_session = False
+	speech_output = "Recommendations for further education include: coursera, khan academy, codecademy, udemy, udacity, and edx."
+	reprompt_text = "you can also ask me what has remained the same"
+	
+	return build_response(session_attributes, build_speechlet_response(card_title, speech_output, reprompt_text, should_end_session))
+
+def jf_frame_change(intent, session):
+	card_title = 'changes'
+	session_attributes = {}
+	should_end_session = False
+
+	speech_output = "the updates to the frameworks include now the four career tracks, an industry-aligned roles, framework of skills with three proficiency levels and training opportunities."
+	reprompt_text = "you can also ask me what has remained the same"
+	
+	return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+        
+def jf_frame_unchange(intent, session):
+	card_title = 'same'
+	session_attributes = {}
+	should_end_session = False
+
+	speech_output = "the parts of the framework that will stay the same are the 10 job families, the corporate titles, global and regional job family heads and the job family will be visible in the hr system. "
+	reprompt_text = "you can also ask me what has changed about the framework"
+	
+	return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+        
+def jf_training_contacts(intent, session):
+	card_title = 'contacts'
+	session_attributes = {}
+	should_end_session = False
+
+	speech_output = "The regional training partners that you can talk to are Zara Shortte who is the head of the e.m.e.a. Region. Eva Zeron who is in charge of the Americas. Arvind Sahu and Rahman Shanawas who both oversee the Asia-Pacific region. Finally there is Silvio Rutsche who oversee Switzerland. Please talk to one of our Regional Job Family Leads to receive their emails and telephone numbers."
+	reprompt_text = "please visit my friendly leads here to collect any information to reach these contacts."
+	
+	return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+        
+def jf_value_staff(intent, session):
+	card_title = 'staff'
+	session_attributes = {}
+	should_end_session = False
+
+	speech_output = "the value proposition and use cases for staff are used to find relevant training opportunities.identify your skill gaps and transferable skills. and much more."
+
+	reprompt_text = "You can also ask how it helps out the managers"
+	
+	return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+    
+def jf_value_manager(intent, session):
+	card_title = 'managers'
+	session_attributes = {}
+	should_end_session = False
+
+	speech_output = "the value proposition and use cases for managers are used to help an employee in your team formulate their p.d.p.. Provide clarity on skills you expect from your staff and much more."
+	reprompt_text = "You can also ask how it helps out the staff"
+	
+	return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+    
+def jf_count(intent, session):
+    card_title = 'familycount'
+    session_attributes = {}
+    should_end_session = False
+    speech_output = "the ten job families are application architecture and development.  business analysis and engineering. business management. i t operations. line management. project management. quality management and testing. service and delivery management. systems architecture and engineering. user and productions support."
+    reprompt_text = "Count"
+	
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+def general_response():
+    card_titile = "gen"
+    session_attributes = {}
+    should_end_session = False
+    speech_output = "I am sorry, I am unable to provide an answer to your question. Please visit one of our regional job family leads that are present here at the expo to acquire a possible answer."
+    reprompt_text = "generic"
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
 # --------------- Events ------------------
 
 def on_session_started(session_started_request, session):
@@ -371,12 +509,28 @@ def on_intent(intent_request, session):
         return job_belong_intent(intent, session)
     elif intent_name == "TrackJobFamilies":
         return track_job_families_intent(intent, session)
+    elif intent_name == "JobCert":
+        return job_cert_intent(intent, session)
+    elif intent_name == "JFExtraHelp":
+        return jf_extra_help(intent, session)
+    elif intent_name == "JFFrameChange":
+        return jf_frame_change(intent, session)
+    elif intent_name == "JFFrameUnchange":
+        return jf_frame_unchange(intent, session)
+    elif intent_name == "JFTrainingContacts":
+        return jf_training_contacts(intent, session)
+    elif intent_name == "JFValueStaff":
+        return jf_value_staff(intent, session)
+    elif intent_name == "JFValueManager":
+        return jf_value_manager(intent, session)
+    elif intent_name == "JFCount":
+        return jf_count(intent, session)
     elif intent_name == "AMAZON.HelpIntent":
         return get_welcome_response()
     elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
         return handle_session_end_request()
     else:
-        raise ValueError("Invalid intent")
+        return general_response()
 
 
 def on_session_ended(session_ended_request, session):
